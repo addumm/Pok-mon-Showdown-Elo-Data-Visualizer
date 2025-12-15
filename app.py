925244051090be45c08440d173d4110858d9c902
Adam Khan <adamjyk23@gmail.com>
2025-12-15 00:54:12 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,127 @@
+from flask import Flask, render_template, redirect, request
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from showdown_client import fetch_current_ratings, ShowdownUnavailableError, ShowdownUserError
+import pandas as pd
+import re
+
+app = Flask(__name__)
+Scss(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///elo.db"
+db = SQLAlchemy(app)
+
+# Data Class
+class PlayerRating(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    userid = db.Column(db.String(18), nullable = False)
+    username = db.Column(db.String(18), nullable = False)
+    format = db.Column(db.String, nullable = False)
+    elo = db.Column(db.Float, nullable = False)
+    gxe = db.Column(db.Float)
+    wins = db.Column(db.Integer)
+    losses = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, default = datetime.now)
+
+    def __repr__(self) -> str:
+        return f"player {self.id}"
+
+# Page
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    # Add user
+    formats = []
+    current_username = None
+    error_message = None
+
+    if request.method == "POST":
+        # parse usernames to alphanumeric
+        current_username = request.form["username"].strip().lower()
+        current_username = re.sub(r'[^a-zA-Z0-9]', '', current_username)
+
+        # handle no input
+        if current_username == "":
+            return render_template("index.html",
+                                   current_username = current_username,
+                                   formats = formats,
+                                   error_message = ""
+                                   )
+
+        # CHECK IF USER IS ALREADY IN DB
+        user_has_rows = (
+        PlayerRating.query.filter_by(userid=current_username).first() is not None
+        )
+
+        if user_has_rows:
+            formats = (
+            db.session.query(PlayerRating.format).filter(PlayerRating.userid == 
+                                                            current_username).distinct().all()
+                                                            )
+            formats = [f[0] for f in formats]
+            return render_template("index.html", 
+                               current_username = current_username, 
+                               formats = formats,
+                               error_message = None
+                               )            
+
+        # if user is not in DB, try to hit showdown api for inputted username
+        try:
+            rating_df = fetch_current_ratings(current_username)
+
+        except ShowdownUserError:
+            error_message = "No user found."
+            return render_template("index.html",
+                                   current_username = None,
+                                   formats = formats,
+                                   error_message=error_message)
+
+        except ShowdownUnavailableError:
+            error_message = "Showdown is temporarily unavailable. Please try again later."
+
+            return render_template("index.html", 
+                                   current_username = None, 
+                                   formats = formats,
+                                   error_message = error_message)
+
+        # add player & stats to db
+        for _, row in rating_df.iterrows():
+            player = PlayerRating(
+            userid = row["userid"],
+            username = row["username"],
+            format = row["format"],
+            elo = float(row["elo"]),
+            gxe = float(row["gxe"]),
+            wins = int(row["w"]),
+            losses = int(row["l"]),
+            timestamp = row["timestamp"],
+            )
+            db.session.add(player)
+        db.session.commit()
+
+        formats = (
+            db.session.query(PlayerRating.format).filter(PlayerRating.userid == 
+                                                            current_username).distinct().all()
+        )
+        formats = [f[0] for f in formats]
+
+        return render_template(
+            "index.html",
+            current_username = current_username,
+            formats=formats,
+            error_message = None
+        )
+        
+    else:
+        return render_template("index.html", 
+                               current_username = current_username, 
+                               formats = formats,
+                               error_message = None
+                               )
+
+
+if __name__ in "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug = True)
---
+++ showdown_client.py
@@ -0,0 +1,51 @@
+import requests
+from requests.exceptions import HTTPError
+import json
+import pandas as pd
+from io import StringIO
+
+class ShowdownUserError(Exception):
+    pass
+
+class ShowdownUnavailableError(Exception):
+    pass
+
+
+def fetch_current_ratings(username: str) -> pd.DataFrame:
+    user_link = "https://pokemonshowdown.com/users/" + username + ".json"
+
+    try:
+        r = requests.get(user_link)
+        r.raise_for_status()
+    except HTTPError as e:
+        status = e.response.status_code
+
+        if status == 403:
+            raise ShowdownUserError("invalid username")
+        
+        elif status == 404:
+            raise ShowdownUserError("user not found")
+        
+        elif status == 503:
+            raise ShowdownUnavailableError("server unavailable")
+
+        else:
+            raise
+        
+    except requests.exceptions.RequestException:
+        raise ShowdownUnavailableError("network error")
+
+    user_dict = json.loads(r.text)
+
+    df_ratings = pd.DataFrame(user_dict["ratings"]).T
+    df_ratings = df_ratings.reset_index().rename(columns = {"index": "format"})
+
+    df = pd.read_json(StringIO(r.text))
+    df = df.reset_index().rename(columns = {"index": "format"})
+
+    df = pd.merge(df, df_ratings, how = "inner", on = "format")
+    df.drop(["ratings", "registertime", "group", "rpr", "rprd", "coil"], axis = 1, inplace = True)
+    df = df[["userid", "username", "format", "elo", "gxe", "w", "l"]]
+    df["timestamp"] = pd.Timestamp.now()
+
+    return df
